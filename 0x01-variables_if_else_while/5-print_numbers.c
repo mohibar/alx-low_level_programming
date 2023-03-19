532d5bc48230a2191536fb24e4353644c0e31591
mohibar <cibaarsom@gmail.com>
2023-03-19 06:30:18 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main - where all code
+  *Return: 0 Always = Sucess
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%d", x);
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
