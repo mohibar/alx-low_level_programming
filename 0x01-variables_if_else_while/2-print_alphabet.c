974aaf8fc41a6101fa67c3f0d69a66f1e77ddc16
mohibar <cibaarsom@gmail.com>
2023-03-19 05:54:35 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main - function for code
+  *Return: 0 Always = Sucess
+  */
+
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; ++a)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
