033321f1d3db528694d9305ac7533ff171eb7afc
mohibar <cibaarsom@gmail.com>
2023-03-19 07:19:34 -0700

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - print two digits combo
+  * Return: 0 Always
+  */
+
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 < 100; n1++)
+	{
+		for (n2 = n1 + 1; n2 < 100; n2++)
+		{
+			putchar((n1 / 10) + '0');
+			putchar((n1 % 10) + '0');
+			putchar(' ');
+			putchar((n2 / 10) + '0');
+			putchar((n2 % 10) + '0');
+			if (n1 == 98 && n2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
