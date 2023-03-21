c9863631e60da6f4ec33e7f363f99ba290e51c1a
mohibar <cibaarsom@gmail.com>
2023-03-21 05:11:37 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - print putchar and new line
+ * putchar prints character
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('p');
+	_putchar('u');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
