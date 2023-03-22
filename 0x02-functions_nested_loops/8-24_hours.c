7d59d435886d59d7fc7537f335ad72a93d49be91
mohibar <cibaarsom@gmail.com>
2023-03-21 18:55:25 -0700

    8-24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * jack_bauer - prints 24h in h and s
+  *
+  * Return: Nothing
+  */
+void jack_bauer(void)
+{
+	int h = 0;
+	int m = 0;
+
+	while (h <= 23)
+	{
+		m = 0;
+		while (m <= 59)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+			m++;
+		}
+		h++;
+	}
+}
